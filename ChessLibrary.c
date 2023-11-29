eb9be05b10a9556eeb4ca08e96f2fd3b356330f2
VIADAROLA <viadarola@wisc.edu>
2023-11-29 15:37:34 -0600

    Pre-Meeting Setup

--- ChessLibrary.c
+++ ChessLibrary.c
@@ -1,1 +1,62 @@
+/**
+ * @file ChessLibrary.c
+ * @author Vinceent Iadarola KHK Expo
+ * @brief
+ * @version 0.1
+ * @date 2023-09-16
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include "ChessLibrary.h"
 
+piece_t board[8][8];
+bool currTurn = true;
+
+/**
+ * @brief This function initialize the board to be the start of a chess match
+ * white peices are row < 1 and Black peices are row > 6
+ *
+ */
+void chessboardInit()
+{
+    // Clears current board of any potential pieces
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+
+            if (i <= 1)
+            {
+                board[i][j].color = true;
+                board[i][j].special_moves = FIRST_MOVE
+            }
+            else if (i >= 6)
+            {
+                board[i][j].color = false;
+                board[i][j].special_moves = FIRST_MOVE
+            }
+            else
+            {
+                board[i][j] = EMPTY;
+            }
+        }
+    }
+
+    // Set up the pawns
+    for (int i = 0; i < 8; i++)
+    {
+        // White pawns
+        board[1][i].piece_type = PAWN;
+
+        // Black pawns
+        board[6][i].piece_type = PAWN;
+    }
+
+    // Setting up peices
+    board[0][0].piece_type = board[0][7].piece_type = board[7][0].piece_type = board[7][7].piece_type = ROOK;
+    board[0][1].piece_type = board[0][6].piece_type = board[7][1].piece_type = board[7][6].piece_type = KNIGHT;
+    board[0][2].piece_type = board[0][5].piece_type = board[7][2].piece_type = board[7][5].piece_type = BISHOP;
+    board[0][3].piece_type = board[7][3].piece_type = QUEEN;
+    board[0][4].piece_type = board[7][4].piece_type = KING;
+}
--- ChessLibrary.h
+++ ChessLibrary.h
@@ -1,1 +1,95 @@
+/**
+ * @file ChessLibrary.h
+ * @author Vincent Iadarola KHK Expo
+ * @brief to do
+ * @version 0.1
+ * @date 2023-11-27
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include "GameEngine.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
+move_t movelist[28];
+bool currTurn;
+
+/**
+ * @brief This enum contai ns every peice type for a chessboard/checkers game
+ * an empty square will be notated by the EMPTY enum then all other values
+ * should be ignored
+ *
+ */
+typedef enum
+{
+    KING,
+    QUEEN,
+    ROOK,
+    KNIGHT,
+    BISHOP,
+    PAWN,
+    EMPTY,
+} chess_piece_name_t;
+
+/**
+ * @brief This enum clarifies which game state the baord is currently running
+ *
+ */
+typedef enum
+{
+    CHESS,
+    WHITE_WINS,
+    BLACK_WINS,
+    STALEMATE,
+} game_state_t;
+
+/**
+ * @brief The special_moves_t is an enum to help determine special case moves in the game running. In chess these cases inlcude en passant,
+ * the double pawn move, and castling. In checkers this would be a promoted piece.
+ *
+ */
+typedef enum
+{
+    EN_PASSANTABLE,
+    FIRST_MOVE,
+    // REDUNDANT BUT COULD HELP READABILITY
+    CASTLEABLE,
+} special_moves_t;
+
+/**
+ * @brief The piece_t holds basic peice information to help determine what properties it has in the game.
+ *
+ */
+typedef struct
+{
+    chess_piece_name_t piece_type;
+    // White is true
+    bool color;
+
+    special_moves_t special_status;
+    // If peice is in your kings safe gaurd vectors
+    bool discover_check;
+} piece_t;
+
+/**
+ * @brief The move_t struct helps pass information to the firmware
+ *
+ */
+typedef struct
+{
+    uint8_t row;
+    uint8_t col;
+    bool isAttacking;
+} move_t;
+
+void chessboardInit();
+
+move_t *getValidMoves(int row, int col);
+
+bool kingStatus();
+
+game_state_t checkGamOver();
