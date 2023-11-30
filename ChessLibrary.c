9c41568019c27917a1ed05befa81edd8ce37cc80
VIADAROLA <viadarola@wisc.edu>
2023-11-30 14:00:06 -0600

    updated params

--- ChessLibrary.c
+++ ChessLibrary.c
@@ -15,12 +15,7 @@
 // Interfile variable
 piece_t board[8][8];
 
-// this shouldnt be referenced in any method just know that the max size is 27
-position_t movelist[27];
-// THE CURRENT TURN/COLOR SHOULD BE PASSED IN FROM THE GAME ENGINE
-
 // Global variables
-position_t kingMoves[3][3];
 position_t whiteKing;
 position_t blackKing;
 
@@ -75,17 +70,9 @@ void chessboardInit()
     board[0][2].piece_type = board[0][5].piece_type = board[7][2].piece_type = board[7][5].piece_type = BISHOP;
     board[0][3].piece_type = board[7][3].piece_type = QUEEN;
     board[0][4].piece_type = board[7][4].piece_type = KING;
-
-    // Sets
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            kingMoves[i][j] = false;
-        }
-    }
-    kingMoves[1][1] = true;
 }
+
+// SIGNIFY UNUSED SPOTS WITH signify unused spots with .row/.col = -1.
 
 /**
  * @brief Pawn Max
@@ -93,7 +80,7 @@ void chessboardInit()
  * @return *position_t
  */
 // Pawn
-static position_t *validPawnMoves()
+static position_t *validPawnMoves(int row, int col, bool currTurn, position_t moveset[28])
 {
     // Implement pawn-specific logic
     return NULL;
@@ -105,7 +92,7 @@ static position_t *validPawnMoves()
  * @return *position_t
  */
 // Knight
-static position_t *validKnightMoves()
+static position_t *validKnightMoves(int row, int col, bool currTurn, position_t moveset[28])
 {
     // Implement knight-specific logic
     return NULL;
@@ -116,7 +103,7 @@ static position_t *validKnightMoves()
  *
  * @return *position_t
  */
-static position_t *validBishopMoves()
+static position_t *validBishopMoves(int row, int col, bool currTurn, position_t moveset[28])
 {
     // Implement bishop-specific logic
     return NULL;
@@ -127,7 +114,7 @@ static position_t *validBishopMoves()
  *
  * @return *position_t
  */
-static position_t *validRookMoves()
+static position_t *validRookMoves(int row, int col, bool currTurn, position_t moveset[28])
 {
     // Implement rook-specific logic
     return NULL;
@@ -138,20 +125,22 @@ static position_t *validRookMoves()
  *
  * @return *position_t
  */
-static position_t *validQueenMoves()
+static position_t *validQueenMoves(int row, int col, bool currTurn, position_t moveset[28])
 {
     // Implement queen-specific logic
     return NULL;
 }
 
 /**
- * @brief Get the Valid Moves piece at row col MICHAEL AND BEN
+ * @brief Get the Valid Moves piece at row col MICHAEL AND BEN the max size of a moveset will
+ * be 28
+ *
  *
  * @param row
  * @param col
  * @return *position_t
  */
-position_t *getValidMoves(int row, int col)
+position_t *getValidMoves(int row, int col, bool currTurn)
 {
     kingCastable();
 }
@@ -164,7 +153,7 @@ position_t *getValidMoves(int row, int col)
  * @return true if castle is a valid move
  * @return false if castling is not a vlid move
  */
-static bool *kingCastable(bool color)
+static bool *kingCastable(bool kingColor)
 {
     return {false, false};
 }
@@ -174,6 +163,7 @@ static bool *kingCastable(bool color)
  * and sets discovered checks flags in the king's outgoing vectors to set discoverCheck
  * flags.
  *
+ * @param kingColor
  */
 void kingStatus(bool kingColor)
 {
@@ -184,11 +174,13 @@ void kingStatus(bool kingColor)
     {
         for (int j = 0; j < 8; j++)
         {
-
             // If board has no peices skip over it
             if (board[i][j].piece_type == EMPTY)
             {
                 continue;
+            }
+            else
+            {
             }
         }
     }
@@ -199,8 +191,8 @@ void kingStatus(bool kingColor)
  * so that it will no go off the board. However, this does not check if there is currently a piece in that position
  * or if it is being attacked
  *
- * @param kingColor
- * @param moveset
+ * @param kingColor the current king be evaulated
+ * @param moveset the array that will be generated with valid moves
  */
 static void getKingPotentialMoves(bool kingColor, position_t moveset[3][3])
 {
--- ChessLibrary.h
+++ ChessLibrary.h
@@ -18,12 +18,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-/**
- * @brief This enum contains every piece type for a chessboard/checkers game
- * an empty square will be notated by the EMPTY enum then all other values
- * should be ignored
- *
- */
 typedef enum
 {
     KING,
@@ -35,10 +29,6 @@ typedef enum
     EMPTY,
 } chess_piece_name_t;
 
-/**
- * @brief The position_t struct helps pass information to the firmware
- *
- */
 typedef struct
 {
     uint8_t row;
@@ -46,51 +36,31 @@ typedef struct
     bool Attacked;
 } position_t;
 
-/**
- * @brief The special_moves_t is an enum to help determine special case moves in the game running. In chess these cases inlcude en passant,
- * the double pawn move, and castling. In checkers this would be a promoted piece.
- *
- */
 typedef enum
 {
     EN_PASSANTABLE,
     FIRST_MOVE,
-    // REDUNDANT BUT COULD HELP READABILITY
     CASTLEABLE,
 } special_moves_t;
 
-/**
- * @brief The piece_t holds basic peice information to help determine what properties it has in the game.
- *
- */
 typedef struct
 {
     chess_piece_name_t piece_type;
-    // White is true
     bool color;
-
     special_moves_t special_status;
-    // If peice is in your kings safe gaurd vectors
     bool discover_check;
 } piece_t;
 
+extern piece_t board[8][8];
+
 void chessboardInit();
 
-position_t *getValidMoves(int row, int col);
+position_t *getValidMoves(int row, int col, bool currTurn);
 
-/**
- * @brief Checks if any enemy pieces can move into the 3x3 array around the king
- * and sets discovered checks flags in the king's outgoing vectors to set discoverCheck
- * flags.
- *
- */
-void kingStatus();
+void kingStatus(bool kingColor);
 
 game_state_t checkGameOver();
 
 position_t pawnPromotions();
-
-piece_t board[8][8];
-position_t movelist[27];
 
 #endif
